14ef89bfa02ea0c6a3fd57e9422430ac3b04e64b
ashourisra <85707708+israashour@users.noreply.github.com>
2025-02-02 14:20:18 +0200

    edits

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -12,7 +12,7 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('package-assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('package-assets/asset/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -153,17 +153,17 @@
     </a>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('package-assets/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('package-assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('package-assets/asset/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('package-assets/asset/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('package-assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('package-assets/asset/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('package-assets/js/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
-    <script src="{{ asset('package-assets/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('package-assets/asset/chart.js/Chart.min.js') }}"></script>
 
     <!-- Page level custom scripts -->
     <script src="{{ asset('package-assets/js/demo/chart-area-demo.js') }}"></script>
